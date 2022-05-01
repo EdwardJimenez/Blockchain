b258ef42095b30ccd3a7027f0cc36b4bf0a81476
Noemi <san2019745@est.univalle.edu>
2022-05-01 15:17:38 -0400

    Copiar los archivos de entidades

---
+++ SWADBlockchain/App_Code/Entidades/EBPerson.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de EBPerson
+/// </summary>
+/// 
+[DataContract]
+public class EBPerson
+{
+    [DataMember]
+    public string IdPerson { get; set; }
+
+    [DataMember]
+    public string fullName { get; set; }
+
+    [DataMember]
+    public string cellphone { get; set; }
+    [DataMember]
+    public DateTime registerDate { get; set; }
+    [DataMember]
+    public DateTime lastUpdate{ get; set; }
+    [DataMember]
+    public string status { get; set; }
+    [DataMember]
+    public string ci { get; set; }
+
+    [DataMember]
+    public string description { get; set; }
+
+    [DataMember]
+    public string IdUser { get; set; }
+    [DataMember]
+    public string ciExtra { get; set; }
+
+    public EBPerson()
+    {
+        IdPerson = string.Empty;
+        fullName = string.Empty;
+        cellphone = string.Empty;
+        registerDate = DateTime.MinValue;
+        lastUpdate = DateTime.MinValue;
+        status = string.Empty;
+        ci = string.Empty;
+        description = string.Empty;
+        IdUser= string.Empty;
+        ciExtra=string.Empty;
+    }
+}
---
+++ SWADBlockchain/App_Code/Entidades/EBToken.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de EBToken
+/// </summary>
+[DataContract]
+public class EBQrToken { 
+    [DataMember]
+    public string idToken { get; set; }
+    [DataMember]
+    public string typeToken { get; set; }
+    [DataMember]
+    public string idTittles { get; set; }
+
+    public EBQrToken()
+    {
+        idToken = string.Empty;
+        typeToken = string.Empty;
+        idTittles = string.Empty;
+    }
+}
