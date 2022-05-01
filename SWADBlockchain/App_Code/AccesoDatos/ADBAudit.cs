6f3bb5e42840bb57a3cacac94948357d1174c8b5
PedroAndresCondeValdez <pedroandrescondevaldez@gmail.com>
2022-05-01 09:44:14 -0400

    Seguimiento de Servicios

---
+++ SWADBlockchain/App_Code/AccesoDatos/ADBAudit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+/// <summary>
+/// Descripción breve de ADBidAudit
+/// </summary>
+public class ADBidAudit
+{
+    public DTOBAudit Obtener_Audit_O_idAudit(string Id_Audit)
+    {
+        //Metodo select por medio del ID
+        DTOBAudit dtoBAudit = new DTOBAudit();
+        try
+        {
+            Database BDSWADBlockchain = SBaseDatos.BDSWADNETIntUn;
+            DbCommand dbCommand = BDSWADBlockchain.GetStoredProcCommand("Audit_O_idAudit"); //PROCESO
+            BDSWADBlockchain.AddInParameter(dbCommand, "idAudit", DbType.String , Id_Audit);
+            BDSWADBlockchain.LoadDataSet(dbCommand, dtoBAudit, "Audit");
+
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+        return dtoBAudit;
+    }
+
+}
