da6cd0f1ca769b1f7508791ce3c9d414c3461c5e
Pedro Andres Conde Valdez <cvp2019844@est.univalle.edu>
2022-06-03 22:31:49 -0400

    Interfaz Ver usuarios NetValle y Metodos para mandar los datos a la Pagina de Registro

---
+++ WABlockchain/WebForm/BVerUsuariosNetValle.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WABlockchain.SWLNBlockchainService;
+
+namespace WABlockchain.WebForm
+{
+    public partial class BVerUsuariosNetValle : System.Web.UI.Page
+    {
+        SWLNBlockchainClient swLNBlockchainClient = new SWLNBlockchainClient();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CargarRequerimientos();
+        }
+        private void CargarRequerimientos()
+        {
+            List<IUserCareerCompleja> lstProgramaRequerimientoCompleja = new List<IUserCareerCompleja>();
+            lstProgramaRequerimientoCompleja = swLNBlockchainClient.U_Obtener_UserCareerComplejas_O_Person().ToList();
+            grvUsersNetvalle.DataSource = lstProgramaRequerimientoCompleja;
+            grvUsersNetvalle.DataBind();
+        }
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            int id = ((GridViewRow)(sender as Control).NamingContainer).RowIndex;
+            int s = int.Parse(grvUsersNetvalle.Rows[id].Cells[0].Text);
+            Session["idUsuario"] = s;
+            Response.Redirect("BRegistroUsuarioNuevo.aspx");
+        }
+    }
+}
