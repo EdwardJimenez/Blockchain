efd319475a9fdfee2f98c5c5df8ad4dd85ef31f7
JoseBascope <BTJ0027390@est.univalle.edu>
2022-06-16 18:25:34 -0400

    Verificar Titulo 100% + interfaz

--- WABlockchain/WebForm/BFormularioBuscarTtulo.aspx.cs
+++ WABlockchain/WebForm/BFormularioBuscarTtulo.aspx.cs
@@ -4,14 +4,52 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WABlockchain.Class;
+using WABlockchain.SWLNBlockchainService;
 
 namespace WABlockchain.WebForm
 {
     public partial class BFormularioBuscarTtulo : System.Web.UI.Page
     {
+        private static Helper _helper = new Helper();
+        SWLNBlockchainClient swLNBlockchainClient = new SWLNBlockchainClient();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            EBTittle tittle = new EBTittle();
+            grvTitulos.DataSource = null;
+            grvTitulos.DataBind();
+            try
+            {
+                string nombreEst;
+                if (txtSegundoApellido.Text != "")
+                {
+                    nombreEst = (txtNombrePersona.Text).Trim() + " " + (txtPrimerApellido.Text).Trim() + " " + (txtSegundoApellido.Text).Trim();
+                }
+                else
+                {
+                    nombreEst = (txtNombrePersona.Text).Trim() + " " + (txtPrimerApellido.Text).Trim();
+                }
+                List<EBTittle> lstTitle = new List<EBTittle>();
+                lstTitle = swLNBlockchainClient.Search_BTitle(nombreEst, txtCarrera.Text, txtFacultad.Text).ToList();
+                if (lstTitle.Count() == 0)
+                {
+                    lblMensaje.Text = "No se encontro el titulo verifica tus datos";
+                }
+                else
+                {
+                    grvTitulos.DataSource = lstTitle;
+                    grvTitulos.DataBind();
+                    grvTitulos.SelectedIndex = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = "No se encontro el titulo verifica tus datos" + ex.Message;
+            }
         }
     }
 }
